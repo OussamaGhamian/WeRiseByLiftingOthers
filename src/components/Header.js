042829ec6b457f617d4d2648539ac7857bd5c696
Sam Ghamian <sam@oussama.localdomain>
2020-02-11 21:21:07 +0200

    fixing style

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ export default class Header extends Component {
                     <ReactBootstrap.Nav.Link ><Link to={'/Home'} className="nav-link">Home</Link> </ReactBootstrap.Nav.Link>
                         <ReactBootstrap.Nav.Link ><Link to={'/Portfolio'} className="nav-link">Portfolio</Link> </ReactBootstrap.Nav.Link>
                         <ReactBootstrap.Nav.Link ><Link to={'/HIT'} className="nav-link">How It Work</Link> </ReactBootstrap.Nav.Link>
-                        <ReactBootstrap.Nav.Link ><Link to={'/ContactUs'} className="nav-link">Contact US</Link> </ReactBootstrap.Nav.Link>\
+                        <ReactBootstrap.Nav.Link ><Link to={'/ContactUs'} className="nav-link">Contact US</Link> </ReactBootstrap.Nav.Link>
                         <ReactBootstrap.Nav.Link ><Link to={'/Services'} className="nav-link">Services</Link> </ReactBootstrap.Nav.Link>
                         <ReactBootstrap.Nav.Link ><Link to={'/Faq'} className="nav-link">FAQ</Link> </ReactBootstrap.Nav.Link>
                     </ReactBootstrap.Nav>
