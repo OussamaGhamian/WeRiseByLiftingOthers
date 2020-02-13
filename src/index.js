eb6dec62f50ce9ce578618b8b988dec261cd0a4c
Sam Ghamian <sam@oussama.localdomain>
2020-02-13 13:42:50 +0200

    fixing bugs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import '@fortawesome/fontawesome-free/css/all.min.css'; 
-import'bootstrap-css-only/css/bootstrap.min.css'; 
-import'mdbreact/dist/css/mdb.css';
-=======
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css'; 
 import 'mdbreact/dist/css/mdb.css';
->>>>>>> ffc93190d6461fa7cd55db27be0054450cd4401d
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
