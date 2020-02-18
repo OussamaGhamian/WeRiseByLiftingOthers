e601ff50b85f49033c49cdc0f43452ceac80e9a8
abdallah2kainassy <abdallahkmainassy@gmail.com>
2020-02-18 09:19:13 +0200

    initialize the back

---
+++ back/src/db.js
@@ -0,0 +1,8 @@
+import sqlite from "sqlite";
+const initializeDatabase = async () => {
+
+
+
+};
+
+export default initializeDatabase;
---
+++ back/src/index.js
@@ -0,0 +1,12 @@
+import app from "./app";
+import initializeDatabase from "./db";
+
+
+const start = async () => {
+
+
+
+
+};
+start();
+
