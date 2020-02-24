cea62b4d42040f617b755479e461fb9ab56c5a6f
abdallah2kainassy <abdallahkmainassy@gmail.com>
2020-02-24 13:45:33 +0200

    git

--- front/src/Components/Services/Card.js
+++ front/src/Components/Services/Card.js
@@ -1,27 +1,23 @@
+import React from "react";
 
-import React from 'react';
+function Card({ card, index }) {
+  {
+    console.log(card);
+  }
+  return (
+    <div className="containar" key={index}>
+      <img
+        className="card-image"
+        src={`http://localhost:8080/${card.image}`}
+        alt=""
+      />
 
-
-
-function Card ({card,index}) {
-  {console.log(card)};
-	return (
-  
-        <div className="containar" key={index}>
-        <img className="card-image"  src={`http://localhost:8080/${card.image}`  } alt="" />
-       
-        <div className="card1">
-          <div className="card-title">
-            {card.title}
-          </div>
-          <div className="card-descition">
-            {card.description}
-          </div>
-          {console.log(card.descition)}
-        </div>
-      
+      <div className="card1">
+        <div className="card-title">{card.title}</div>
+        <div className="card-descition">{card.description}</div>
       </div>
-	)
+    </div>
+  );
 }
 
 export default Card;
