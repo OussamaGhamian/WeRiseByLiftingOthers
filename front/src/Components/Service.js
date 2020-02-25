e13b3d7c16d1d4240585c545cf3663d49e905a80
OussamaGhamian <oussamaalghamian@gmail.com>
2020-02-25 09:11:53 +0200

    unknown changes

--- front/src/Components/Service.js
+++ front/src/Components/Service.js
@@ -1,32 +1,5 @@
 import React from 'react'
-import { useIntersection } from 'react-use'
-// import gsap from 'gsap'
 export default function Service(props) {
-    // const sectionRef = React.useRef(null)
-    // const intersection = useIntersection(sectionRef,
-    //     {
-    //         root: null,
-    //         rootMargin: "0px",
-    //         threshold: 1
-    //     })
-    // const fadeIn = (element) => {
-    //     gsap.to(element, .1, {
-    //         opacity: 1,
-    //         y: -60,
-    //         ease: 'power4.out',
-    //         stagger: {
-    //             amount: .1
-    //         }
-    //     })
-    // }
-    // const fadeOut = (element) => {
-    //     gsap.to(element, 1, {
-    //         opacity: 0,
-    //         y: -2,
-    //         ease: 'power4.out'
-    //     })
-    // }
-    // intersection && intersection.intersectionRatio < 1 ? fadeOut('.fade') : fadeIn('.fade')
     return (
         <div className="service" >
             <img src={`http://localhost:8080/${props.image}`} alt="serivceImage" />
